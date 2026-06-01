0b547e09bbfd5373254b570367406be011be252f
MatutiCode <matutebarrios2020@gmail.com>
2026-06-01 13:35:16 -0300

    se agrego funcion de las cards

--- assets/js/APP.js
+++ assets/js/APP.js
@@ -10,3 +10,25 @@ const personajes = [
 { id: 5, nombre: "Abraxas", imagen:
 "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/5-abraxas.jpg" },
 ];
+function renderCards(lista) {
+    const contenedor = document.getElementById("contenedorCards");
+    contenedor.innerHTML = "";
+    lista.forEach ((personaje) => {
+        const col = document.createElement("div");
+        col.className = "col-4 my-2";
+        col.innerHTML = `
+        <div class="card w-100 h-100" style="width: 18rem">
+            <img
+              src="https://i.pinimg.com/originals/c3/e1/d3/c3e1d329648a2461a8e479e396d2d58c.gif"
+              class="card-img-top"
+              alt="mi gif"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">guiso</p>
+              <a href="#" class="btn btn-primary">god</a>
+            </div>
+          </div>
+          `;
+    })
+}
